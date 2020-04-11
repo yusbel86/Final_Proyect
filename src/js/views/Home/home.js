9d415067483992b4c3b12744448de1a524ebafc8
yusbel86 <yusbel1086@gmail.com>
2020-04-11 16:15:56 +0000

    Added Card

--- src/js/views/Home/home.js
+++ src/js/views/Home/home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Card } from "../../component/Card/card";
 import "../../styles/home.scss";
 
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Team!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
+		<Card 
+          
+        />
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
